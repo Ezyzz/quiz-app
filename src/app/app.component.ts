ad30e718994ae73a27d02b516a5d51895f3668c5
Apiruk Sriruk <sktballkung02@gmail.com>
2023-07-31 15:05:45 +0700

    test 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { quizData } from './data/quiz_data';
+import { Choice } from './question';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  question = {
-    text: 'Who painted the Mona Lisa?',
-    choices:[
-      'Pablo Picasso',
-      'Leonardo Da Vinci',
-      'Vinceent van Gogh',
-      'Michelangelo'
-    ],
+  title(title: any) {
+    throw new Error('Method not implemented.');
   }
-  onClickChoice(text:string){ 
-    console.log(text);
-   }
+  audio = new Audio();
+
+  questions = quizData;
+  currentQuestionIndex = 0;
+  isEnd = false;
+  score = 0;
+
+  constructor() {
+    this.audio.src = './assets/audio/click.wav';
+    this.newQuiz();
+  }
+
+  onClickChoice(choice: Choice) {
+    console.log('User clicked ' + choice.text);
+
+    this.playSound();
+
+    if (choice.isAnswer) this.score++;
+
+    if (this.currentQuestionIndex < this.questions.length - 1) {
+      this.currentQuestionIndex++;
+      this.questions[this.currentQuestionIndex].choices.sort((a, b) => 0.5 - Math.random());
+    } else {
+      this.isEnd = true;
+    }
+  }
+
+  private playSound() { 
+    this.audio.load();
+    this.audio.addEventListener('canplaythrough', () => {
+      this.audio.play();
+    });
+  }
+
+  onClickNewQuiz() {
+    this.newQuiz();
+  }
+
+  private newQuiz() {
+    this.questions.sort((a, b) => 0.5 - Math.random());
+    this.isEnd = false;
+    this.currentQuestionIndex = 0;
+    this.score = 0;
+  }
 }
